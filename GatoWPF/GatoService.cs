6d40a321800fd7af790268c5ca00350096ed9a98
Jorge Esteban Zaragoza <thesuperjez@gmail.com>
2015-09-23 23:05:31 -0500

    Commit Inicial

---
+++ GatoWPF/GatoService.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Windows.Controls;
+namespace GatoWPF
+{
+    class GatoService
+    {
+        public Int16 jugador;
+        public Int16 turnoActual;
+        public char[,] tablero;
+        public Boolean ganador;
+        ListBox consola;
+        Int16 dificultad;
+        public GatoService(ListBox consola)
+        {
+            this.consola = consola;
+        }
+
+        public void iniciarPartida(Int16 dificultad)
+        {
+            this.dificultad = dificultad;
+            string dificultadStr="";
+            switch (dificultad)
+            {
+                case 0: dificultadStr = "Dificil"; break;
+                case 1: dificultadStr = "Medio"; break;
+                case 2: dificultadStr = "Facil"; break;
+            }
+            consola.Items.Add("Inicializando en dificultad: "+dificultadStr);
+            ganador = false;
+            tablero=new char[3,3];
+            Random r = new Random(DateTime.Now.Millisecond);
+            this.jugador = Convert.ToInt16(r.Next(1,3));
+            consola.Items.Add("El jugador es: " + (jugador==1?'X':'O'));
+            turnoActual = 1;
+            if (jugador == 2)
+            {
+                tirarCpu();
+            }
+            
+        }
+
+        public void cambiarTurno()
+        {
+            consola.Items.Add("Cambiando de turno");
+            if(turnoActual==1){
+                turnoActual = 2;
+            }else{
+                turnoActual=1;
+            }
+        }
+       
+
+        public void tirarCpu()
+        {
+            consola.Items.Add("CPU Debe tirar");
+            List<char[,]> listaTableros = new List<char[,]>();
+            for(Int16 i =0;i<3;i++){
+                for (Int16 j = 0; j < 3;j++ )
+                {
+                    char[,] tableroContador = (char[,])tablero.Clone();
+                    if (tableroContador[i, j]==0)
+                    {
+                        tableroContador[i, j] = obtenerPiezaCpu();
+                        listaTableros.Add(tableroContador);
+                    }
+                }
+            }
+            if (listaTableros.Count != 0)
+            {
+                tablero = determinarMovimientoOptimo(dificultad, listaTableros);
+                cambiarTurno();
+            }
+        }
+
+        public char[,] determinarMovimientoOptimo(Int16 dificultad,List<char[,]> listaTableros)
+        {
+            List<Int32> listaProbabilidades = new List<Int32>();
+            foreach(char[,] tableroTemp in listaTableros){
+                    listaProbabilidades.Add(obtenerProbabilidadMin((char[,])tableroTemp.Clone()));
+            }
+            //Burbuja
+            for (Int32 i = 0; i < listaProbabilidades.Count-1;i++)
+            {
+                for (Int32 j = 0; j < listaProbabilidades.Count-1; j++)
+                {
+                    if(listaProbabilidades[j]>listaProbabilidades[j+1]){
+                        Int32 temp = listaProbabilidades[j];
+                        char[,] tableroTemp=listaTableros[j];
+                        listaProbabilidades[j] = listaProbabilidades[j + 1];
+                        listaProbabilidades[j + 1] = temp;
+                        listaTableros[j] = listaTableros[j + 1];
+                        listaTableros[j + 1] = tableroTemp;
+                    }
+                }
+            }
+            if (jugador == 1)
+            {
+                consola.Items.Add("Tirar MIN");
+                //Min
+                if ((0 + dificultad) < 2)
+                {
+                    return listaTableros[(0 + dificultad)];
+                }
+                else
+                {
+                    return listaTableros[0];
+                }
+                
+            }
+            else
+            {
+                consola.Items.Add("Tirar MAX");
+                //Max
+                if (((listaTableros.Count - 1) - dificultad) > 0)
+                {
+                    return listaTableros[((listaTableros.Count - 1) - dificultad)];
+                }
+                else
+                {
+                    return listaTableros[(listaTableros.Count - 1)];
+                }
+                
+            }
+        }
+        public Int32 obtenerProbabilidadMin(char[,] tableroContador)
+        {
+            char[,] copiaTablero = (char[,])tableroContador.Clone();
+            char piezaJugador;
+            if (jugador == 1)
+            {
+                piezaJugador = 'X';
+            }
+            else
+            {
+                piezaJugador = 'O';
+            }
+            for (Int16 i = 0; i < 3; i++)
+            {
+                for (Int16 j = 0; j < 3; j++)
+                {
+                    if(tableroContador[i,j]==0){
+                        tableroContador[i, j] = obtenerPiezaCpu();
+                    }
+                }
+            }
+            
+            for (Int16 i = 0; i < 3; i++)
+            {
+                for (Int16 j = 0; j < 3; j++)
+                {
+                    if (copiaTablero[i, j] == 0)
+                    {
+                        copiaTablero[i, j] = piezaJugador;
+                    }
+                }
+            }
+            Int32 resultado = (contarGanadas(obtenerPiezaCpu(), tableroContador) - contarGanadas(piezaJugador, copiaTablero));
+            consola.Items.Add("Probabilidad de ganar con tiro: " + resultado);
+            return (resultado);
+
+        }
+
+        public Int32 contarGanadas(char pieza, char[,] tablero)
+        {
+            Int32 contador=0;
+            //Horizontal
+            if (tablero[0, 0] == pieza && tablero[0, 1] == pieza && tablero[0, 2] == pieza)
+            {
+                contador++;
+            }
+
+            if (tablero[1, 0] == pieza && tablero[1, 1] == pieza && tablero[1, 2] == pieza)
+            {
+                contador++;
+            }
+            if (tablero[2, 0] == pieza && tablero[2, 1] == pieza && tablero[2, 2] == pieza)
+            {
+                contador++;
+            }
+
+            //Vertical
+            if (tablero[0, 0] == pieza && tablero[1, 0] == pieza && tablero[2, 0] == pieza)
+            {
+                contador++;
+            }
+
+            if (tablero[0, 1] == pieza && tablero[1, 1] == pieza && tablero[2, 1] == pieza)
+            {
+                contador++;
+            }
+            if (tablero[0, 2] == pieza && tablero[1, 2] == pieza && tablero[2, 2] == pieza)
+            {
+                contador++;
+            }
+
+            //Diagonales
+            if (tablero[0, 0] == pieza && tablero[1, 1] == pieza && tablero[2, 2] == pieza)
+            {
+                contador++;
+            }
+            if (tablero[2, 0] == pieza && tablero[1, 1] == pieza && tablero[0, 2] == pieza)
+            {
+                contador++;
+            }
+            
+            return contador;
+        }
+
+
+        public char obtenerPiezaCpu() {
+            if (jugador == 1)
+            {
+                return 'O';
+            }
+            else
+            {
+                return 'X';
+            }
+        }
+
+        public Boolean tirarJugador(Int32 x, Int32 y)
+        {
+            consola.Items.Add("El jugador ha tirado en: " + x+","+y);
+            Boolean retorno=false;
+            if (turnoActual == jugador)
+            {
+                if (jugador == 1)
+                {
+                    tablero[x,y] = 'X';
+                }
+                else
+                {
+                    tablero[x,y] = 'O';
+                }
+                retorno=true;
+            }
+            else
+            {
+                retorno=false;
+            }
+            cambiarTurno();
+            return retorno;
+        }
+
+        
+
+
+    }
+}
---
+++ GatoWPF/MainWindow.xaml.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace GatoWPF
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        GatoService gato;
+        Boolean ganador;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+
+            ganador = false;
+            gato = new GatoService(ConsolaTxt);
+            ((Storyboard)FindResource("iniciar")).Begin(this);
+            ConsolaTxt.Items.Add("Presione Iniciar para empezar el juego");
+        }
+
+        private void Storyboard_Completed_1(object sender, EventArgs e)
+        {
+            ((Storyboard)FindResource("fadeIn")).Begin(TituloLbl);
+            ((Storyboard)FindResource("fadeIn")).Begin(DificultadLbl);
+            ((Storyboard)FindResource("fadeIn")).Begin(DificultadCmb);
+            ((Storyboard)FindResource("fadeIn")).Begin(IniciarBtn);
+            ((Storyboard)FindResource("fadeIn")).Begin(DescripcionLbl);
+            ((Storyboard)FindResource("fadeIn")).Begin(ConsolaLbl);
+            ((Storyboard)FindResource("fadeIn")).Begin(ConsolaTxt);
+        }
+
+        private void Label_MouseRightButtonDown_1(object sender, MouseButtonEventArgs e)
+        {
+           
+        }
+
+        private void Label_MouseLeftButtonDown_1(object sender, MouseButtonEventArgs e)
+        {
+           Application.Current.Shutdown();
+        }
+
+        private void Label01_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            tirar(0,1,sender);
+
+        }
+
+        private void Label_MouseEnter_1(object sender, MouseEventArgs e)
+        {
+            if (((Label)sender).Background.ToString() != "System.Windows.Media.ImageBrush")
+            {
+                ((Storyboard)FindResource("mouseEnter")).Begin((Label)sender);
+            }
+            
+        }
+
+        private void Label_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (((Label)sender).Background.ToString() != "System.Windows.Media.ImageBrush")
+            {
+                ((Storyboard)FindResource("mouseLeave")).Begin((Label)sender);
+            }
+        }
+
+        private void Label_MouseEnter_2(object sender, MouseEventArgs e)
+        {
+            ((Storyboard)FindResource("mouseEnterSalir")).Begin((Label)sender);
+        }
+
+        private void Label_MouseLeave_1(object sender, MouseEventArgs e)
+        {
+            ((Storyboard)FindResource("mouseLeaveSalir")).Begin((Label)sender);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ganador = false;
+            ConsolaTxt.Items.Clear();
+            ((Storyboard)FindResource("fadeIn")).Begin(tablero);
+            Label00.Visibility = Visibility.Visible;
+            Label01.Visibility = Visibility.Visible;
+            Label02.Visibility = Visibility.Visible;
+            Label10.Visibility = Visibility.Visible;
+            Label11.Visibility = Visibility.Visible;
+            Label12.Visibility = Visibility.Visible;
+            Label21.Visibility = Visibility.Visible;
+            Label20.Visibility = Visibility.Visible;
+            Label22.Visibility = Visibility.Visible;
+
+            Label00.Background = new SolidColorBrush(Colors.Transparent);
+            Label01.Background = new SolidColorBrush(Colors.Transparent); ;
+            Label02.Background = new SolidColorBrush(Colors.Transparent); ;
+            Label10.Background = new SolidColorBrush(Colors.Transparent); ;
+            Label11.Background = new SolidColorBrush(Colors.Transparent);
+            Label12.Background = new SolidColorBrush(Colors.Transparent); ;
+            Label20.Background = new SolidColorBrush(Colors.Transparent); ;
+            Label21.Background = new SolidColorBrush(Colors.Transparent); ;
+            Label22.Background = new SolidColorBrush(Colors.Transparent); ;
+
+            gato.iniciarPartida(Convert.ToInt16(DificultadCmb.SelectedValue));
+            actualizarTablero();
+        }
+
+        public void actualizarTablero()
+        {
+
+            for (Int32 i = 0; i < 3;i++ )
+            {
+                for (Int32 j = 0; j < 3; j++)
+                {
+                    //00
+                    if (i == 0 && j == 0 && gato.tablero[i, j]!=0)
+                    {
+                        Label00.Background = new ImageBrush(new BitmapImage(new Uri(@gato.tablero[i, j]+".png", UriKind.Relative)));
+                    }
+                    //01
+                    if (i == 0 && j == 1 && gato.tablero[i, j] != 0)
+                    {
+                        Label01.Background = new ImageBrush(new BitmapImage(new Uri(@gato.tablero[i, j] + ".png", UriKind.Relative)));
+                    }
+                    //02
+                    if (i == 0 && j == 2 && gato.tablero[i, j] != 0)
+                    {
+                        Label02.Background = new ImageBrush(new BitmapImage(new Uri(@gato.tablero[i, j] + ".png", UriKind.Relative)));
+                    }
+
+                    //10
+                    if (i == 1 && j == 0 && gato.tablero[i, j] != 0)
+                    {
+                        Label10.Background = new ImageBrush(new BitmapImage(new Uri(@gato.tablero[i, j] + ".png", UriKind.Relative)));
+                    }
+                    //11
+                    if (i == 1 && j == 1 && gato.tablero[i, j] != 0)
+                    {
+                        Label11.Background = new ImageBrush(new BitmapImage(new Uri(@gato.tablero[i, j] + ".png", UriKind.Relative)));
+                    }
+                    //12
+                    if (i == 1 && j == 2 && gato.tablero[i, j] != 0)
+                    {
+                        Label12.Background = new ImageBrush(new BitmapImage(new Uri(@gato.tablero[i, j] + ".png", UriKind.Relative)));
+                    }
+                    //20
+                    if (i == 2 && j == 0 && gato.tablero[i, j] != 0)
+                    {
+                        Label20.Background = new ImageBrush(new BitmapImage(new Uri(@gato.tablero[i, j] + ".png", UriKind.Relative)));
+                    }
+                    //21
+                    if (i == 2 && j == 1 && gato.tablero[i, j] != 0)
+                    {
+                        Label21.Background = new ImageBrush(new BitmapImage(new Uri(@gato.tablero[i, j] + ".png", UriKind.Relative)));
+                    }
+                    //22
+                    if (i == 2 && j == 2 && gato.tablero[i, j] != 0)
+                    {
+                        Label22.Background = new ImageBrush(new BitmapImage(new Uri(@gato.tablero[i, j] + ".png", UriKind.Relative)));
+                    }
+                }
+            }
+            comprobarGanador('X');
+            comprobarGanador('O');
+        }
+
+        public void comprobarGanador(char pieza)
+        {
+            if (gato.tablero[0, 0] == pieza && gato.tablero[0, 1] == pieza && gato.tablero[0, 2] == pieza)
+            {
+                setGanador(pieza);
+            }
+
+            if (gato.tablero[1, 0] == pieza && gato.tablero[1, 1] == pieza && gato.tablero[1, 2] == pieza)
+            {
+                setGanador(pieza);
+            }
+            if (gato.tablero[2, 0] == pieza && gato.tablero[2, 1] == pieza && gato.tablero[2, 2] == pieza)
+            {
+                setGanador(pieza);
+            }
+
+            //Vertical
+            if (gato.tablero[0, 0] == pieza && gato.tablero[1, 0] == pieza && gato.tablero[2, 0] == pieza)
+            {
+                setGanador(pieza);
+            }
+
+            if (gato.tablero[0, 1] == pieza && gato.tablero[1, 1] == pieza && gato.tablero[2, 1] == pieza)
+            {
+                setGanador(pieza);
+            }
+            if (gato.tablero[0, 2] == pieza && gato.tablero[1, 2] == pieza && gato.tablero[2, 2] == pieza)
+            {
+                setGanador(pieza);
+            }
+
+            //Diagonales
+            if (gato.tablero[0, 0] == pieza && gato.tablero[1, 1] == pieza && gato.tablero[2, 2] == pieza)
+            {
+                setGanador(pieza);
+            }
+            if (gato.tablero[2, 0] == pieza && gato.tablero[1, 1] == pieza && gato.tablero[0, 2] == pieza)
+            {
+                setGanador(pieza);
+            }
+        }
+
+        public void setGanador(char pieza)
+        {
+            ConsolaTxt.Items.Add("Ganador: "+ pieza);
+            MessageBox.Show("Han ganado " + pieza, "Hay un ganador", MessageBoxButton.OK, MessageBoxImage.Information);
+            Label00.Visibility = Visibility.Hidden;
+            Label01.Visibility = Visibility.Hidden;
+            Label02.Visibility = Visibility.Hidden;
+            Label10.Visibility = Visibility.Hidden;
+            Label11.Visibility = Visibility.Hidden;
+            Label12.Visibility = Visibility.Hidden;
+            Label21.Visibility = Visibility.Hidden;
+            Label20.Visibility = Visibility.Hidden;
+            Label22.Visibility = Visibility.Hidden;
+            
+            ganador = true;
+        }
+
+        public void tirar(Int32 x, Int32 y, Object sender)
+        {
+            if (((Label)sender).Background.ToString() != "System.Windows.Media.ImageBrush")
+            {
+                gato.tirarJugador(x, y);
+                if(!ganador){
+                gato.tirarCpu();
+                }
+                actualizarTablero();
+            }
+        }
+
+        private void Label00_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            tirar(0, 0, sender);
+        }
+
+        private void Label02_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            tirar(0, 2, sender);
+        }
+
+        private void Label10_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            tirar(1, 0, sender);
+        }
+
+        private void Label11_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            tirar(1, 1, sender);
+        }
+
+        private void Label12_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            tirar(1, 2, sender);
+        }
+
+        private void Label20_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            tirar(2, 0, sender);
+        }
+
+        private void Label21_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            tirar(2, 1, sender);
+        }
+
+        private void Label22_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            tirar(2, 2, sender);
+        }
+
+        private void Window_MouseDown_1(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                if (e.ChangedButton == MouseButton.Left)
+                {
+                    this.DragMove();
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
